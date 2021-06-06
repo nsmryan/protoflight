e40dc58141955f3ee5189b3a1d754b7cd974dee4
Noah Ryan <nsmryan@gmail.com>
2021-06-06 09:54:22 -0400

    Fixed unused parameters in tbl

--- fsw/src/tbl/tbl.c
+++ fsw/src/tbl/tbl.c
@@ -27,41 +27,44 @@ FSW_RESULT_ENUM tbl_initialize(void)
 }
 
 TBL_RESULT_ENUM tbl_retrieve_table(TBL_TABLE_ID_ENUM table_id,
-                                 TBL_LOCATION_ENUM location,
-                                 uint8_t *table_buffer,
-                                 uint32_t table_size)
+                                   TBL_LOCATION_ENUM location,
+                                   uint8_t *table_buffer,
+                                   uint32_t table_size)
 {
-  TBL_RESULT_ENUM tbl_result = TBL_RESULT_OKAY;
+    (void)table_id;
+    (void)table_size;
+    (void)location;
+    TBL_RESULT_ENUM tbl_result = TBL_RESULT_OKAY;
 
-  if (table_buffer == NULL)
-  {
-    tbl_result = TBL_RESULT_NULL_POINTER;
-  }
-
-  if (tbl_result == TBL_RESULT_OKAY)
-  {
-    switch (location)
+    if (table_buffer == NULL)
     {
-      case TBL_LOCATION_INVALID:
-        tbl_result = TBL_RESULT_INVALID_ARGUMENT;
-        break;
-
-      case TBL_LOCATION_STORED:
-        // NOTE provide stored copy
-        break;
-
-      case TBL_LOCATION_RAM:
-        // NOTE provide RAM copy
-        break;
-
-      default:
-        tbl_result = TBL_RESULT_INVALID_ARGUMENT;
-        break;
+        tbl_result = TBL_RESULT_NULL_POINTER;
     }
-  }
-  // NOTE probably need hardware to implement stored memory tables
 
-  return tbl_result;
+    if (tbl_result == TBL_RESULT_OKAY)
+    {
+        switch (location)
+        {
+            case TBL_LOCATION_INVALID:
+                tbl_result = TBL_RESULT_INVALID_ARGUMENT;
+                break;
+
+            case TBL_LOCATION_STORED:
+                // NOTE provide stored copy
+                break;
+
+            case TBL_LOCATION_RAM:
+                // NOTE provide RAM copy
+                break;
+
+            default:
+                tbl_result = TBL_RESULT_INVALID_ARGUMENT;
+                break;
+        }
+    }
+    // NOTE probably need hardware to implement stored memory tables
+
+    return tbl_result;
 }
 
 TBL_RESULT_ENUM tbl_update_table(TBL_TABLE_ID_ENUM table_id,
@@ -69,17 +72,17 @@ TBL_RESULT_ENUM tbl_update_table(TBL_TABLE_ID_ENUM table_id,
                                  uint8_t *table_buffer,
                                  uint32_t table_size)
 {
-  TBL_RESULT_ENUM tbl_result = TBL_RESULT_OKAY;
+    TBL_RESULT_ENUM tbl_result = TBL_RESULT_OKAY;
 
-  // NOTE probably need hardware to implement stored memory tables
+    // NOTE probably need hardware to implement stored memory tables
 
-  return tbl_result;
+    return tbl_result;
 }
 
 void tbl_get_status(TBL_Status *status)
 {
-  if (status != NULL)
-  {
-    *status = gvTBL_state.status;
-  }
+    if (status != NULL)
+    {
+        *status = gvTBL_state.status;
+    }
 }
